10c1512a41bfe5aecdc55f84f5ea6dd2530828be
paultrantelus <53578826+paultrantelus@users.noreply.github.com>
2024-08-21 15:32:58 -0400

    Variables

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project = "th-ptranlab-lab-6134d2"
-  region  = "us-west1"
-  zone    = "us-west1-a"
+  project = var.project_id
+  region  = var.region
+  zone    = "${var.region}-a"
 }
