46a204a905ef6b1b653e50f09ca57b7a4b06904c
paultrantelus <53578826+paultrantelus@users.noreply.github.com>
2024-08-21 16:32:15 -0400

    website

--- main.tf
+++ main.tf
@@ -30,18 +30,6 @@ module "app_network" {
   ]
 }
 
-resource "google_compute_network" "app" {
-  name                    = var.network_name
-  auto_create_subnetworks = false
-}
-
-resource "google_compute_subnetwork" "app" {
-  name          = var.network_name
-  ip_cidr_range = var.network_ip_range
-  region        = var.region
-  network       = google_compute_network.app.id
-}
-
 data "google_compute_image" "ubuntu" {
   most_recent = true
   project     = var.image_project 
@@ -52,7 +40,9 @@ resource "google_compute_instance" "blog" {
   name         = var.app_name
   machine_type = var.machine_type
 
-    boot_disk {
+  tags = ["${var.network_name}-web"]
+
+  boot_disk {
     initialize_params {
       image = data.google_compute_image.ubuntu.self_link
     }
@@ -64,5 +54,6 @@ resource "google_compute_instance" "blog" {
     }
   }  
 
+  metadata_startup_script = "apt -u update;apt -y install nginx; echo ${var.app_name} > /var/www/html/index.html"
   allow_stopping_for_update = true
 }
